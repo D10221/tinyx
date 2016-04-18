1bc82a58d1556bd1a57be565ce7db5d1d6faa48e
dan <danieliozzi@gmail.com>
2016-04-18 19:32:12 +0200

    removed .idea, typings, dist

--- typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="assertion-error/assertion-error.d.ts" />
-/// <reference path="chai/chai.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="fetch/fetch.js.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
